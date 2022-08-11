a02a2db0423055d7dc53ce1860e9eb79d1a351c4
Facundo Martirena <facundo.martirena@estudiantes.utec.edu.uy>
2022-08-11 20:46:11 -0300

    comitNumeroMil

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import './App.css';
+import Start from './paginas/Start'
+import Inicio from './paginas/Inicio'
+import PaginaPokemon from './paginas/PaginaPokemon'
+import "@fontsource/poppins";
+
+function App() {
+
+
+  return (
+    <div className='w-full letra  m-auto h-full'>
+   <BrowserRouter>
+    <Routes>
+      <Route 
+       path='/'
+       element={<Start 
+       
+       />}
+      />
+      <Route 
+       path='/Inicio'
+       element={<Inicio 
+       
+       />}
+      />
+      <Route 
+       path='/PaginaPokemon/:id'
+       element={<PaginaPokemon
+       
+       />}
+      />
+    </Routes>
+   </BrowserRouter>
+   </div>
+  )
+}
+
+export default App
